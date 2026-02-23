9fa475cb286d1aa74f3ccf6c08e87f7d075b1e95
Yashsowdam <yashsowdam91@gmail.com>
2026-02-23 16:57:48 +1100

    Prepare frontend for production API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import { getAccessToken, getRefreshToken, setTokens, clearTokens } from "../auth
 import { clear } from "@testing-library/user-event/dist/clear";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.REACT_APP_API_URL,
 })
 
 api.interceptors.request.use((config) => {
